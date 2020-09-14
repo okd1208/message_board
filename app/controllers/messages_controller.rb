8869e5da77b92c72d9fe93f2593ec1287c9ad930
yutaro.okada <y.okada.upd@gmail.com>
2020-09-14 05:01:45 +0000

    add pagination

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy]
 
     def index
-        @messages = Message.all
+        @messages = Message.all.page(params[:page])
   end
 
   def show
@@ -52,6 +52,6 @@ class MessagesController < ApplicationController
   end
   
   def message_params
-      params.require(:message).permit(:content)
+      params.require(:message).permit(:content, :title)
   end
 end
